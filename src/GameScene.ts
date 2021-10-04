4de44a8b0bdfcbf5b4ff20eaeb41e86475941732
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-10-04 00:45:44 -0400

    update bg

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -395,6 +395,7 @@ export class GameScene extends GameObject {
 		this.uiOverlay.texture = tex('restart');
 		await this.delay(500);
 		await this.fire();
+		this.bg.texture = tex(randItem(['bg_b', 'bg_c']));
 		this.uiOverlay.texture = tex('blank');
 		this.whiteout();
 		this.makeProps();
